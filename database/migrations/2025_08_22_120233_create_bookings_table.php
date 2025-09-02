53d18bbf5cfb0226cc596ff34487622481a262af
NurCholisss <nurc11700@gmail.com>
2025-09-02 14:09:15 +0700

    first commit

---
+++ database/migrations/2025_08_22_120233_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('vehicle_id')->constrained()->onDelete('cascade');
+            $table->foreignId('service_id')->nullable()->constrained()->onDelete('set null');
+            $table->date('schedule_date');
+            $table->time('schedule_time');
+            $table->text('customer_complaint');
+            $table->text('system_recommendation')->nullable();
+            $table->enum('status', ['pending', 'approved', 'done', 'cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
